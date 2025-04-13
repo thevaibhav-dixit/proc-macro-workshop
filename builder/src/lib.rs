459b42df7b863597c6fcca7ef3e321d29b98c8cd
Vaibhav <thevaibhavdixit@gmail.com>
2025-04-13 14:30:30 +0530

    feat: first builder test passes

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // parse the input token into a syntax tree
+    let _input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    TokenStream::new()
 }
