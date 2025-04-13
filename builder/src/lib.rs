2a350fd6bbeca89536ffbd3cf6c3b7da26f4cd9b
Vaibhav <thevaibhavdixit@gmail.com>
2025-04-13 16:27:45 +0530

    feat: third builder test passes

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -10,26 +10,58 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     let builder_ident = syn::Ident::new(&format!("{}Builder", name), name.span());
 
+    // trying to get the fields
+    let fields = match input.data {
+        syn::Data::Struct(ref data) => match data.fields {
+            syn::Fields::Named(ref fields) => &fields.named,
+            _ => panic!("Only named fields are supported"),
+        },
+        _ => panic!("Only structs are supported"),
+    };
+
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            #name: Option<#ty>,
+        }
+    });
+
+    let builder_init = fields.iter().map(|f| {
+        let name = &f.ident;
+        quote! {
+            #name: None,
+        }
+    });
+
+    let setters = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        quote! {
+            pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                self.#name = Some(#name);
+                self
+            }
+        }
+    });
+
     let expanded = quote! {
 
         pub struct #builder_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+            #(#builder_fields)*
         }
 
         impl #name {
             pub fn builder() -> #builder_ident {
                 #builder_ident {
-                    executable: None,
-                    args: None,
-                    env: None,
-                    current_dir: None,
+                    #(#builder_init)*
                 }
             }
         }
 
+        impl #builder_ident {
+            #(#setters)*
+        }
 
     };
 
