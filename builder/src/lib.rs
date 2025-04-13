9e51b1426f57a1130468863c60c0f211d55edf50
Vaibhav <thevaibhavdixit@gmail.com>
2025-04-13 17:21:22 +0530

    feat: 04/05/06 builder test done

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, DeriveInput, PathArguments, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -45,6 +45,28 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     });
 
+    fn is_option(ty: &Type) -> bool {
+        if let Type::Path(type_path) = ty {
+            if let Some(segment) = type_path.path.segments.first() {
+                return segment.ident == "Option"
+                    && matches!(segment.arguments, PathArguments::AngleBracketed(_));
+            }
+        }
+        false
+    }
+    let build_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+        if is_option(ty) {
+            return quote! {
+                #name: self.#name.take(),
+            };
+        }
+        quote! {
+            #name: self.#name.take().ok_or_else(|| format!("Field {} is not set", stringify!(#name)))?,
+        }
+    });
+
     let expanded = quote! {
 
         pub struct #builder_ident {
@@ -61,6 +83,12 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
         impl #builder_ident {
             #(#setters)*
+
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    #(#build_fields)*
+                })
+            }
         }
 
     };
