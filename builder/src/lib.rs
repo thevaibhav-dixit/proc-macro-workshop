d8acf82964987cacf21b545ac530dff72723bc47
Vaibhav <thevaibhavdixit@gmail.com>
2025-04-13 15:41:36 +0530

    feat: second builder test passes

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,21 @@
 use proc_macro::TokenStream;
+use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     // parse the input token into a syntax tree
-    let _input = parse_macro_input!(input as DeriveInput);
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
 
-    TokenStream::new()
+    let builder_ident = syn::Ident::new(&format!("{}Builder", name), name.span());
+
+    let expanded = quote! {
+        impl #name {
+            pub fn builder() -> () {}
+        }
+
+    };
+
+    TokenStream::from(expanded)
 }
