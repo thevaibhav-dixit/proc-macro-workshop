386c229c6c196e2fc5d30c7c4528b5ac86c4b013
Vaibhav <thevaibhavdixit@gmail.com>
2025-05-02 01:18:32 +0530

    feat: 08 builder test passes

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,13 +4,10 @@ use syn::{parse_macro_input, DeriveInput, PathArguments, Type};
 
 #[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    // parse the input token into a syntax tree
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
-
     let builder_ident = syn::Ident::new(&format!("{}Builder", name), name.span());
 
-    // trying to get the fields
     let fields = match input.data {
         syn::Data::Struct(ref data) => match data.fields {
             syn::Fields::Named(ref fields) => &fields.named,
@@ -19,7 +16,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
         _ => panic!("Only structs are supported"),
     };
 
-    fn get_each_attr(field: &syn::Field) -> Option<syn::Ident> {
+    struct FieldInfo<'a> {
+        name: syn::Ident,
+        ty: &'a Type,
+        each_attr: Option<syn::Ident>,
+    }
+
+    fn get_each_attr(field: &syn::Field) -> Result<Option<syn::Ident>, proc_macro2::TokenStream> {
         for attr in &field.attrs {
             if attr.path().is_ident("builder") {
                 let mut each_value = None;
@@ -28,21 +31,34 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     if meta.path.is_ident("each") {
                         let value = meta.value()?;
                         let string_value = value.parse::<syn::LitStr>()?;
-                        each_value = Some(syn::Ident::new(
-                            string_value.value().as_str(),
-                            string_value.span(),
-                        ));
-                        return Ok(());
+                        each_value =
+                            Some(syn::Ident::new(&string_value.value(), string_value.span()));
+                        Ok(())
+                    } else {
+                        Err(syn::Error::new_spanned(
+                            attr.meta.clone(),
+                            "expected `builder(each = \"...\")`",
+                        ))
                     }
-                    Ok(())
                 });
 
-                if result.is_ok() && each_value.is_some() {
-                    return each_value;
-                }
+                return match result {
+                    Ok(_) => Ok(each_value),
+                    Err(err) => Err(err.to_compile_error()),
+                };
             }
         }
-        None
+        Ok(None)
+    }
+
+    fn is_option(ty: &Type) -> bool {
+        if let Type::Path(type_path) = ty {
+            if let Some(segment) = type_path.path.segments.first() {
+                return segment.ident == "Option"
+                    && matches!(segment.arguments, PathArguments::AngleBracketed(_));
+            }
+        }
+        false
     }
 
     fn inner_type_of_vec(ty: &Type) -> &Type {
@@ -75,102 +91,92 @@ pub fn derive(input: TokenStream) -> TokenStream {
         ty
     }
 
-    let builder_fields = fields.iter().map(|f| {
-        let name = &f.ident;
+    let mut field_infos = Vec::new();
+
+    for f in fields {
+        let name = f.ident.clone().expect("Expected named field");
         let ty = &f.ty;
 
-        if get_each_attr(f).is_some() {
-            let inner_ty = inner_type_of_vec(ty);
-            return quote! {
-                #name: Vec<#inner_ty>,
-            };
-        }
-        if is_option(ty) {
-            return quote! {
-                #name: #ty,
-            };
-        }
-        quote! {
-            #name: Option<#ty>,
+        let each_attr = match get_each_attr(f) {
+            Ok(attr) => attr,
+            Err(err) => return TokenStream::from(err),
+        };
+
+        field_infos.push(FieldInfo {
+            name,
+            ty,
+            each_attr,
+        });
+    }
+
+    let builder_fields = field_infos.iter().map(|info| {
+        let name = &info.name;
+        if info.each_attr.is_some() {
+            let inner_ty = inner_type_of_vec(info.ty);
+            quote! { #name: Vec<#inner_ty>, }
+        } else if is_option(info.ty) {
+            let ty = info.ty;
+            quote! { #name: #ty, }
+        } else {
+            let ty = info.ty;
+            quote! { #name: Option<#ty>, }
         }
     });
 
-    let builder_init = fields.iter().map(|f| {
-        let name = &f.ident;
-        if get_each_attr(f).is_some() {
-            return quote! {
-                #name: Vec::new(),
-            };
-        }
-
-        quote! {
-            #name: None,
+    let builder_init = field_infos.iter().map(|info| {
+        let name = &info.name;
+        if info.each_attr.is_some() {
+            quote! { #name: Vec::new(), }
+        } else {
+            quote! { #name: None, }
         }
     });
 
-    let setters = fields.iter().map(|f| {
-        let name = &f.ident;
-        let ty = &f.ty;
+    let setters = field_infos.iter().map(|info| {
+        let name = &info.name;
+        let ty = info.ty;
 
-        if get_each_attr(f).is_some() {
+        if let Some(each_name) = &info.each_attr {
             let inner_ty = inner_type_of_vec(ty);
-            let each_name = get_each_attr(f).expect("name should be present");
-            return quote! {
+            quote! {
                 pub fn #each_name(&mut self, #each_name: #inner_ty) -> &mut Self {
                     self.#name.push(#each_name);
-                    self
-                }
-            };
-        }
-
-        if is_option(ty) {
+                    self }
+            }
+        } else if is_option(ty) {
             let inner_ty = inner_type_of_option(ty);
-            return quote! {
+            quote! {
                 pub fn #name(&mut self, #name: #inner_ty) -> &mut Self {
                     self.#name = Some(#name);
                     self
                 }
-            };
-        }
-        quote! {
-            pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                self.#name = Some(#name);
-                self
+            }
+        } else {
+            quote! {
+                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
             }
         }
     });
 
-    fn is_option(ty: &Type) -> bool {
-        if let Type::Path(type_path) = ty {
-            if let Some(segment) = type_path.path.segments.first() {
-                return segment.ident == "Option"
-                    && matches!(segment.arguments, PathArguments::AngleBracketed(_));
+    let build_fields = field_infos.iter().map(|info| {
+        let name = &info.name;
+        let ty = info.ty;
+
+        if info.each_attr.is_some() {
+            quote! { #name: self.#name.clone(), }
+        } else if is_option(ty) {
+            quote! { #name: self.#name.take(), }
+        } else {
+            quote! {
+                #name: self.#name.take().ok_or_else(|| format!("Field {} is not set", stringify!(#name)))?,
             }
-        }
-        false
-    }
-    let build_fields = fields.iter().map(|f| {
-        let name = &f.ident;
-        let ty = &f.ty;
-
-        if get_each_attr(f).is_some() {
-            return quote! {
-                #name: self.#name.clone(),
-            };
-        }
-
-        if is_option(ty) {
-            return quote! {
-                #name: self.#name.take(),
-            };
-        }
-        quote! {
-            #name: self.#name.take().ok_or_else(|| format!("Field {} is not set", stringify!(#name)))?,
         }
     });
 
     let expanded = quote! {
-
         pub struct #builder_ident {
             #(#builder_fields)*
         }
@@ -192,7 +198,6 @@ pub fn derive(input: TokenStream) -> TokenStream {
                 })
             }
         }
-
     };
 
     TokenStream::from(expanded)
